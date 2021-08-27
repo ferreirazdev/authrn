6db1f171ce0bc4d9c4d7020c0845ba9bb3d131fb
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-08-27 07:41:28 -0300

    some auth features and adding async storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import 'react-native-gesture-handler';
 import React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+
+import { AuthProvider } from "./contexts/auth";
+import Routes from "./routes";
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>Teste</Text>
-      </View>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 };
 
-export default App
+export default App;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,26 @@
+import React, {useContext} from 'react'
+import { View, Button, StyleSheet } from "react-native";
+import AuthContext from '../../contexts/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useContext(AuthContext);
+
+  function handleSignOut(){
+    signOut()
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign Out" onPress={handleSignOut} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
+
+export default Dashboard;
